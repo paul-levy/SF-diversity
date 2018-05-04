c87b37de99dceb30ed26b8f098c4ed218824ac0e
paul-levy <paul.levy@nyu.edu>
2018-05-03 23:16:09 -0400

    replaced tabs with spaces in helper_fcns to help work on local machine

--- altExp/analysis/functions/helper_fcns.py
+++ altExp/analysis/functions/helper_fcns.py
@@ -234,15 +234,15 @@ def fit_CRF(cons, resps, nr_c50, nr_expn, nr_gain, nr_base, v_varGain, fit_type)
 	# evaluate the model
         pred = naka_rushton(cons[sf], nr_args); # ensure we don't have pred (lambda) = 0 --> log will "blow up"
         
-	if fit_type == 4:
+        if fit_type == 4:
 	    # Get predicted spike count distributions
-	  mu  = pred; # The predicted mean spike count; respModel[iR]
+          mu  = pred; # The predicted mean spike count; respModel[iR]
           var = mu + (v_varGain * np.power(mu, 2));                        # The corresponding variance of the spike count
           r   = np.power(mu, 2) / (var - mu);                           # The parameters r and p of the negative binomial distribution
           p   = r/(r + mu);
 	# no elif/else
 
-	if fit_type == 1 or fit_type == 2:
+        if fit_type == 1 or fit_type == 2:
 		# error calculation
           if fit_type == 1:
             loss = lambda resp, pred: np.sum(np.power(resp-pred, 2)); # least-squares, for now...
@@ -252,14 +252,14 @@ def fit_CRF(cons, resps, nr_c50, nr_expn, nr_gain, nr_base, v_varGain, fit_type)
           curr_loss = loss(resps[sf], pred);
           loss_by_sf[sf] = np.sum(curr_loss);
 
-	else:
+        else:
 		# if likelihood calculation
           if fit_type == 3:
             loss = lambda resp, pred: poisson.logpmf(resp, pred);
             curr_loss = loss(resps[sf], pred); # already log
           if fit_type == 4:
             loss = lambda resp, r, p: np.log(nbinom.pmf(resp, r, p)); # Likelihood for each pass under doubly stochastic model
-	    curr_loss = loss(resps[sf], r, p); # already log
+            curr_loss = loss(resps[sf], r, p); # already log
           loss_by_sf[sf] = -np.sum(curr_loss); # negate if LLH
 
     return np.sum(loss_by_sf);
