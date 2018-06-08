9df92d208b79d39af2f04911f71ea36a62c933c0
Paul Levy <paul.gerald.levy@gmail.com>
2018-06-08 16:32:14 -0400

    same only-pick-training-subsample-once adjustment as in altExp/ branch; most recent fit lists, changes to subsampling rate in batch scripts

--- Analysis/tf-func/tfFits-thresh.s
+++ Analysis/tf-func/tfFits-thresh.s
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --time=15:00:00
-#SBATCH --mem=4GB
+#SBATCH --time=5:00:00
+#SBATCH --mem=16GB
 
 #SBATCH --job-name=princeTF
 
 #SBATCH --mail-user=pl1465@nyu.edu
 #SBATCH --mail-type=ALL
 
-#SBATCH --output=tfSq_%A_%a.out
-#SBATCH --error=tfSq_%A_%a.err
+#SBATCH --output=tf_%A_%a.out
+#SBATCH --error=tf_%A_%a.err
 
 module purge
 source /home/pl1465/SF_diversity/Analysis/tf2.7/python2.7.12/bin/activate
@@ -20,7 +20,7 @@ source /home/pl1465/SF_diversity/Analysis/tf2.7/python2.7.12/bin/activate
 #     2 - poisson
 #     3 - modPoiss
 #   subset_frac = 0 means take all data (no subsampling)
-python mod_resp_trackNLL.py $SLURM_ARRAY_TASK_ID 1e-8 0.001 1 0.10 0
+python mod_resp_trackNLL.py $SLURM_ARRAY_TASK_ID 1e-8 0.1 2 0.9 0
  
 # leave a blank line at the end
 
