Discovered serious bug!!! The loss function used was based entirely off of the normalization type specified; now corrected

LGN/analysis/tf-func/mod_resp_trackNLL.py
@@ -8,7 +8,7 @@ from time import sleep
 import sys
 
 sys.path.insert(0, '../functions/');
-from helper_fcns import np_smart_load
+from helper_fcns import np_smart_load, organize_adj_responses
 
 import tensorflow as tf
 
@@ -345,15 +345,15 @@ def SFMGiveBof(ph_stimOr, ph_stimTf, ph_stimCo, ph_stimSf, ph_stimPh, ph_spikeCo
     meanRate      = tf.reduce_mean(ratio, axis=1);
     respModel     = noiseLate + (scale * meanRate);
 
-    if fitType == 1:
+    if lossType == 1:
       # alternative loss function: just (sqrt(modResp) - sqrt(neurResp))^2
       lsq = tf.square(tf.add(tf.sqrt(respModel), -tf.sqrt(ph_spikeCount)));
       NLL = tf.reduce_mean(ph_objWeight*lsq); # was 1*lsq
-    elif fitType == 2:
+    elif lossType == 2:
         # must be same type for using tf.nn.log_poisson_loss, so typecast
       log_lh = tf.nn.log_poisson_loss(tf.cast(ph_spikeCount, dtype=tf.float32), tf.cast(tf.log(respModel), dtype=tf.float32));
       NLL = tf.reduce_mean(1*log_lh); # nn.log_poisson_loss already negates!
-    elif fitType == 3:
+    elif lossType == 3:
       # Get predicted spike count distributions
       mu  = tf.multiply(ph_stimDur, respModel); 
       #mu  = tf.maximum(tf.constant(.01, dtype=tf.float32), tf.multiply(ph_stimDur, respModel)); 
@@ -441,7 +441,7 @@ def setModel(cellNum, stopThresh, lr, lossType = 1, fitType=1, subset_frac = 1, 
     loc_data = '/home/pl1465/SF_diversity/LGN/analysis/structures/'; # Prince cluster
     #loc_data = '/Users/paulgerald/work/sfDiversity/sfDiv-OriModel/sfDiv-python/LGN/analysis/structures/'; # Personal mac
 
-    fL_name = 'fitList_180927';
+    fL_name = 'fitList_180930';
     # fitType
     if fitType == 1:
       fL_suffix1 = '_flat';
@@ -466,6 +466,7 @@ def setModel(cellNum, stopThresh, lr, lossType = 1, fitType=1, subset_frac = 1, 
     dataNames = dataList['unitName'];
 
     S = np_smart_load(str(loc_data + dataNames[cellNum-1] + '_sfm.npy')); # why -1? 0 indexing...
+    rvcFits = np_smart_load(str(loc_data + 'rvcFits_pos.npy'));
     trial_inf = S['sfm']['exp']['trial'];
     #prefOrEst = mode(trial_inf['ori'][1]).mode;
     prefSfEst = gmean(trial_inf['sf'][0][~numpy.isnan(trial_inf['sf'][0])]); # avoid NaN trials...
@@ -565,7 +566,9 @@ def setModel(cellNum, stopThresh, lr, lossType = 1, fitType=1, subset_frac = 1, 
     fixedPh = stimPh[:,~mask];
     
     # cell responses
-    spikes = [numpy.sum(x) for x in trial_inf['power_f1'][~mask]];
+    _, spks = organize_adj_responses(S, rvcFits[cellNum-1]);
+    spikes = spks[~mask];
+    #spikes = [numpy.sum(x) for x in trial_inf['power_f1'][~mask]];
     stim_dur = trial_inf['duration'][~mask];
     objWeight = objWeight[~mask];        
 
@@ -629,6 +632,12 @@ def setModel(cellNum, stopThresh, lr, lossType = 1, fitType=1, subset_frac = 1, 
     m = tf.Session();
     init = tf.global_variables_initializer();
     m.run(init);
+
+    # compute initial loss
+    NLL = m.run(okok, feed_dict={ph_stimOr: fixedOr, ph_stimTf: fixedTf, ph_stimCo: fixedCo, \
+                ph_stimSf: fixedSf, ph_stimPh: fixedPh, ph_spikeCount: spikes, \
+                ph_stimDur: stim_dur, ph_objWeight:objWeight, ph_normResp: normResp, ph_normCentSf: normCentSf});
+    print('\n\n*** Initial loss: %.3f ***\n' % NLL);
     
     # guaranteed to have NLL for this cell by now (even if just placeholder)
     currNLL = fitList[cellNum-1]['NLL'];

altExp/analysis/tf-func/mod_resp_trackNLL.py
@@ -378,15 +378,15 @@ def SFMGiveBof(ph_stimOr, ph_stimTf, ph_stimCo, ph_stimSf, ph_stimPh, ph_spikeCo
     meanRate      = tf.reduce_mean(ratio, axis=1);
     respModel     = noiseLate + (scale * meanRate);
 
-    if fitType == 1:
+    if lossType == 1:
       # alternative loss function: just (sqrt(modResp) - sqrt(neurResp))^2
       lsq = tf.square(tf.add(tf.sqrt(respModel), -tf.sqrt(ph_spikeCount)));
       NLL = tf.reduce_mean(ph_objWeight*lsq); # was 1*lsq
-    elif fitType == 2:
+    elif lossType == 2:
         # must be same type for using tf.nn.log_poisson_loss, so typecast
       log_lh = tf.nn.log_poisson_loss(tf.cast(ph_spikeCount, dtype=tf.float32), tf.cast(tf.log(respModel), dtype=tf.float32));
       NLL = tf.reduce_mean(1*log_lh); # nn.log_poisson_loss already negates!
-    elif fitType == 3:
+    elif lossType == 3:
       # Get predicted spike count distributions
       mu  = tf.multiply(ph_stimDur, respModel); 
       #mu  = tf.maximum(tf.constant(.01, dtype=tf.float32), tf.multiply(ph_stimDur, respModel)); 