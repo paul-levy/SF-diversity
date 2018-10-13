added new loss function (variance explained), changed sf plots to rescale per contrast (not fixed axis), etc

LGN/sach/descr_fit.py
@@ -22,6 +22,7 @@ def descr_loss(params, f1, all_sfs, contrast, loss_type = 3, DoGmodel = 1):
                2 - sqrt
                3 - Sach sum{[(exp-obs)^2]/[k+sigma^2]} where
                    k := 0.01*max(obs); sigma := measured variance of the response
+               4 - variance explained!
     '''
     obs_mean = f1['mean'][contrast, :];
     obs_sem = f1['sem'][contrast, :];
@@ -45,10 +46,15 @@ def descr_loss(params, f1, all_sfs, contrast, loss_type = 3, DoGmodel = 1):
       NLL = NLL + loss;
     elif loss_type == 3:
       k = 0.01*np.max(obs_mean);
+      #sigma = np.ones_like(obs_mean);
       sigma = obs_sem;
       sq_err = np.square(obs_mean-pred_mean);
       NLL = NLL + np.sum(sq_err/(k+np.square(sigma)));
-
+    elif loss_type == 4:
+      resp_dist = lambda x, y: np.sum(np.square(x-y))/np.maximum(len(x), len(y))
+      var_expl = lambda m, r, rr: 100 * (1 - resp_dist(m, r)/resp_dist(r, rr));
+      # rr is the mean of all observed mean responses (and is last argument in below line)
+      NLL = NLL - var_expl(pred_mean, obs_mean, np.mean(obs_mean) * np.ones_like(obs_mean));
     #print('NLL %.2f || params %s' % (NLL, str(params)));
     
     return NLL;
@@ -61,13 +67,15 @@ def fit_descr_DoG(cell_num, data_loc, n_repeats = 4, loss_type = 3, DoGmodel = 1
     dataList = hf.np_smart_load(data_loc + 'sachData.npy');
     assert dataList!=[], "data file not found!"
 
-    fLname = 'descrFits_181012';
+    fLname = 'descrFits_d181012';
     if loss_type == 1:
       loss_str = '_poiss';
     elif loss_type == 2:
       loss_str = '_sqrt';
     elif loss_type == 3:
       loss_str = '_sach';
+    elif loss_type == 4:
+      loss_str = '_varExpl';
     if DoGmodel == 1:
       mod_str = '_sach';
     elif DoGmodel == 2:
@@ -129,10 +137,10 @@ def fit_descr_DoG(cell_num, data_loc, n_repeats = 4, loss_type = 3, DoGmodel = 1
         for n_try in range(n_repeats):
           # pick initial params
           if DoGmodel == 1:
-            init_gainCent = hf.random_in_range((maxResp, 2*maxResp))[0];
-            init_radiusCent = hf.random_in_range((0.05, 0.3))[0];
-            init_gainSurr = init_gainCent * hf.random_in_range((0.2, 0.4))[0];
-            init_radiusSurr = 3*init_radiusCent;
+            init_gainCent = hf.random_in_range((maxResp, 5*maxResp))[0];
+            init_radiusCent = hf.random_in_range((0.05, 2))[0];
+            init_gainSurr = init_gainCent * hf.random_in_range((0.1, 0.8))[0];
+            init_radiusSurr = hf.random_in_range((0.5, 4))[0];
             init_params = [init_gainCent, init_radiusCent, init_gainSurr, init_radiusSurr];
           elif DoGmodel == 2:
             init_gainCent = maxResp * hf.random_in_range((0.9, 1.2))[0];
@@ -159,7 +167,7 @@ def fit_descr_DoG(cell_num, data_loc, n_repeats = 4, loss_type = 3, DoGmodel = 1
               currParams[con, :] = params;
               varExpl[con] = hf.var_explained(data, params, con, DoGmodel);
               prefSf[con] = hf.dog_prefSf(params, all_sfs, DoGmodel);
-              charFreq[con] = hf.charFreq(params, DoGmodel);
+              charFreq[con] = hf.dog_charFreq(params, DoGmodel);
 
     # update stuff - load again in case some other run has saved/made changes
     if os.path.isfile(fLname):

LGN/sach/helper_fcns.py
@@ -15,6 +15,7 @@ import pdb
 # dog_prefSf - compute the prefSF given a DoG model/fit
 # dog_prefSfMod - smooth prefSf vs. contrast with a functional form/fit
 # charFreq - given a model/parameter set, return the characteristic frequency of the tuning curve
+# charFreqMod - smooth characteristic frequency vs. contrast with a functional form/fit
 
 # deriv_gauss - evaluate a derivative of a gaussian, specifying the derivative order and peak
 # compute_SF_BW - returns the log bandwidth for height H given a fit with parameters and height H (e.g. half-height)
@@ -161,7 +162,7 @@ def dog_prefSfMod(descrFit, allCons, varThresh=65):
 
   return pSfRatio, psf_model, opt_params;
 
-def charFreq(prms, DoGmodel=1):
+def dog_charFreq(prms, DoGmodel=1):
   if DoGmodel == 1:
       r_c = prms[1];
       f_c = 1/(np.pi*r_c)
@@ -169,6 +170,37 @@ def charFreq(prms, DoGmodel=1):
       f_c = prms[1];
 
   return f_c;
+
+def dog_charFreqMod(descrFit, allCons, varThresh=65):
+  ''' Given a descrFit dict for a cell, compute a fit for the prefSf as a function of contrast
+      Return ratio of prefSf at highest:lowest contrast, lambda of model, params
+  '''
+  # the model
+  fc_model = lambda offset, slope, alpha, con: offset + slope*np.power(con-con[0], alpha);
+  # gather the values
+  #   only include prefSf values derived from a descrFit whose variance explained is gt the thresh
+  validInds = np.where(descrFit['varExpl'][:] > varThresh)[0];
+  if len(validInds) == 0: # i.e. no good fits...
+    return np.nan, [], [];
+  prefSfs = descrFit['charFreq'][disp, validInds];
+  conVals = allCons[validInds];
+  weights = descrFit['varExpl'][disp, validInds];
+  # set up the optimization
+  obj = lambda params: np.sum(np.multiply(weights,
+        np.square(fc_model(params[0], params[1], params[2], conVals) - prefSfs)))
+  init_offset = charFreqs[0];
+  conRange = conVals[-1] - conVals[0];
+  init_slope = (charFreqs[-1] - charFreqs[0]) / conRange;
+  init_alpha = 0.4; # most tend to be saturation (i.e. contrast exp < 1)
+  # run
+  optz = opt.minimize(obj, [init_offset, init_slope, init_alpha], bounds=((0, None), (None, None), (0.25, 4)));
+  opt_params = optz['x'];
+  # ratio:
+  extrema = fc_model(*opt_params, con=(conVals[0], conVals[-1]))
+  fcRatio = extrema[-1] / extrema[0]
+
+  return fcRatio, fc_model, opt_params;
+
 
 #####
 

LGN/sach/plotting.py
@@ -103,7 +103,7 @@ for c in reversed(range(nCons)):
 
 ax.set_aspect('equal', 'box'); 
 ax.set_xlim((0.5*np.min(all_sfs[val_sfs]), 1.2*np.max(all_sfs[val_sfs])));
-ax.set_ylim((5e-2, 1.5*maxResp));
+#ax.set_ylim((5e-2, 1.5*maxResp));
 
 #ax.set_xscale('symlog', linthreshx=all_sfs[1]); # all_sfs[0] = 0, but all_sfs[0]>1
 ax.set_xscale('log');
@@ -152,7 +152,7 @@ for c in reversed(range(nCons)):
       sfsAx[c_plt_ind].plot(sfs_plot, helper_fcns.DoGsach(*curr_mod_params, stim_sf=sfs_plot)[0], clip_on=False)
     elif sf_DoG_model == 2:
       sfsAx[c_plt_ind].plot(sfs_plot, helper_fcns.DiffOfGauss(*curr_mod_params, stim_sf=sfs_plot)[0], clip_on=False)
-    f_c = helper_fcns.charFreq(curr_mod_params, sf_DoG_model);
+    f_c = helper_fcns.dog_charFreq(curr_mod_params, sf_DoG_model);
     # note we take DiffOfGaus(.)[0] since that is the unnormalized version of the DoG response
 
     sfsAx[c_plt_ind].set_xlim((min(sfs_plot), max(sfs_plot)));
@@ -166,7 +166,7 @@ for c in reversed(range(nCons)):
     sfsAx[c_plt_ind].tick_params(width=1, length=4, which='minor', direction='out'); # minor ticks, too...	
     sns.despine(ax=sfsAx[c_plt_ind], offset=10, trim=False); 
 
-    sfsAx[c_plt_ind].set_ylim((0, 1.5*maxResp));
+    #sfsAx[c_plt_ind].set_ylim((0, 1.5*maxResp));
     yAxStr = 'response ';
     sfsAx[c_plt_ind].set_ylabel(str(yAxStr + '(spikes/s)'));
 