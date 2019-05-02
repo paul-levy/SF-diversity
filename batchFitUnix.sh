ebfe29b926def3078d93aa92c3a25a891e1d8668
Paul Levy <paul.gerald.levy@gmail.com>
2019-05-02 15:24:31 -0400

    added optional flag in batchFitUnix for tracking opt across steps; newest animate.ipynb

--- batchFitUnix.sh
+++ batchFitUnix.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# params are: cellNum, expDir, lossType, fitType, initFromCurr
+# params are: cellNum, expDir, lossType, fitType, initFromCurr, trackSteps
 # see model_responses.py for additional details
 
 source activate lcv-python
 
-for run in {1..34}; do
-  python model_responses.py $run LGN/ 4 1 1 &
-  python model_responses.py $run LGN/ 4 2 1 &
+for run in {1..59}; do
+  python model_responses.py $run V1_orig/ 4 1 0 1 &
+  python model_responses.py $run V1_orig/ 4 2 0 1 &
 done
