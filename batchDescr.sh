e70053c6d23b1ed98045ef020c9348287c74f816
Paul Levy <paul.gerald.levy@gmail.com>
2019-05-05 04:14:25 -0400

    adjusted/made comment re. multiple disps in batchDescr

--- batchDescr.sh
+++ batchDescr.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+### README
+# Have you set the dataList name?
+# Have you set the phAdv name?
+# Have you set the RVC name?#
+# Have you set the descriptive fit name?
+# Have you set the modelRecovery status/type?
+### Go to descr_fits.py first
+
 # arguments are
 #   1 - cell #
 #   2 - dispersion (index into the list of dispersions for that cell; not used in descr/DoG fits)
@@ -7,17 +15,18 @@
 #   4 - make phase advance fits
 #   5 - make RVC fits
 #   6 - make descriptive (DoG) fits (1 or 0)
-#   7 - DoG model (sach or tony)
+#   7 - DoG model (flexGauss [0; not DoG] or sach [1] or tony [2])
 #   [8 - phase direction (pos or neg)]; default is pos (check...)
 #   [9 - regularization for gain term (>0 means penalize for high gain)] default is 0
 
 source activate lcv-python
 
-for run in {1..5}
+# NOTE: If running only SF descr fits, do not need to run separately for all disp
+
+for run in {1..17}
 do
-  # phAdv fits
-  # rvcFits
-  # DoG fits
-  python descr_fits.py $run 0 V1/ 1 1 0 0 -1 & 
-  python descr_fits.py $run 0 V1/ 1 1 0 0 1 & 
+  python descr_fits.py $run 0 V1/ 0 0 1 0 1 & 
+  #python descr_fits.py $run 1 V1/ 0 0 1 0 1 & 
+  #python descr_fits.py $run 2 V1/ 0 0 1 0 1 & 
+  #python descr_fits.py $run 3 V1/ 0 0 1 0 1 & 
 done
