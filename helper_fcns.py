added empirical c50 calculation for RVC fits (hf); (hf) fixed conGain calc to work for rvcMod=0; in plot_descr, fixed c50 plotting, colors/labels

helper_fcns.py
@@ -72,6 +72,7 @@ import warnings
 # DoGsach - difference of gaussians as implemented in sach's thesis
 # var_explained - compute the variance explained for a given model fit/set of responses
 # chiSq      - compute modified chiSq loss value as described in Cavanaugh et al
+# c50_empirical - compute the effective/emperical c50 by optimization
 # dog_prefSf - compute the prefSf for a given DoG model/parameter set
 # dog_prefSfMod - fit a simple model of prefSf as f'n of contrast
 # dog_charFreq - given a model/parameter set, return the characteristic frequency of the tuning curve
@@ -433,15 +434,21 @@ def descrFit_name(lossType, descrBase=None, modelName = None):
     
   return descrName;
 
-def rvc_fit_name(rvcBase, modNum, dir):
-   ''' returns the correct suffix for the given RVC model number and direction (pos/neg)
-   '''
+def rvc_mod_suff(modNum):
+   ''' returns the suffix for a given rvcModel number'''
    if modNum == 0:
      suff = '';
    elif modNum == 1:
      suff = '_NR';
    elif modNum == 2:
      suff = '_peirce';
+   
+   return suff;
+
+def rvc_fit_name(rvcBase, modNum, dir):
+   ''' returns the correct suffix for the given RVC model number and direction (pos/neg)
+   '''
+   suff = rvc_mod_suff(modNum);
 
    base = rvcBase + suff;
 
@@ -1101,7 +1108,7 @@ def rvc_fit(amps, cons, var = None, n_repeats = 1000, mod=0, fix_baseline=False,
          else:
            b_bounds = (None, 0);
          k_bounds = (0, None);
-         c0_bounds = (3e-2, 1);
+         c0_bounds = (1e-2, 1);
          all_bounds = (b_bounds, k_bounds, c0_bounds); # set all bounds
        elif mod == 1 or mod == 2: # bad initialization as of now...
          if fix_baseline: # correct if we're fixing the baseline at 0
@@ -1137,20 +1144,20 @@ def rvc_fit(amps, cons, var = None, n_repeats = 1000, mod=0, fix_baseline=False,
          best_params = opt_params;
 
        # now determine the contrast gain
-       '''
-       b = opt_params[0]; k = opt_params[1]; c0 = opt_params[2];
-       if b < 0: 
-         # find the contrast value at which the rvc_model crosses/reaches 0
-         obj_whenR0 = lambda con: np.square(0 - rvc_model(b, k, c0, con));
-         con_bound = (0, 1);
-         init_r0cross = 0;
-         r0_cross = opt.minimize(obj_whenR0, init_r0cross, bounds=(con_bound, ));
-         con_r0 = r0_cross['x'];
-         conGain = k/(c0*(1+con_r0/c0));
+       if mod == 0:
+         b = opt_params[0]; k = opt_params[1]; c0 = opt_params[2];
+         if b < 0: 
+           # find the contrast value at which the rvc_model crosses/reaches 0
+           obj_whenR0 = lambda con: np.square(0 - rvc_model(b, k, c0, con));
+           con_bound = (0, 1);
+           init_r0cross = 0;
+           r0_cross = opt.minimize(obj_whenR0, init_r0cross, bounds=(con_bound, ));
+           con_r0 = r0_cross['x'];
+           conGain = k/(c0*(1+con_r0/c0));
+         else:
+           conGain = k/c0;
        else:
-         conGain = k/c0;
-       '''
-       conGain = -100;
+         conGain = -100;
 
      all_opts.append(best_params);
      all_loss.append(best_loss);
@@ -1232,6 +1239,30 @@ def chiSq(data_resps, model_resps, stimDur=1, kMult = 0.10):
   chi = np.sum(np.divide(np.square(num[valid]), k + data_resp_recenter*rho/stimDur));
 
   return chi;
+
+def c50_empirical(rvcMod, params):
+  ''' given a response-versus-contrast model and associated parameters, find the empirical c50
+        i.e. what is the contrast at which is the response 50% of the maximum
+             evaluted over contrast [0, 1]
+  '''
+  con_bound = (0, 1);
+  # first, find the maximum response
+  if rvcMod == 1 or rvcMod == 2: # naka-rushton/peirce
+    obj = lambda con: -naka_rushton(con, params)
+  elif rvcMod == 0: # i.e. movshon form
+    rvcModel = get_rvc_model();
+    obj = lambda con: -rvcModel(*params, con);
+  max_opt = opt.minimize(obj, 0.8, bounds=(con_bound, ));
+  max_response = -max_opt['fun'];
+  max_con = max_opt['x'];
+
+  # now, find out the contrast with 50% of max response
+  con_bound = (0, max_con); # i.e. ensure the c50_emp contrast is less than the max (relevant for super-saturating curves)
+  c50_obj = lambda con: numpy.square(max_response/2 + obj(con));
+  c50_opt = opt.minimize(c50_obj, max_con/2, bounds=(con_bound, ));
+  c50_con = c50_opt['x'];
+  
+  return c50_con;
 
 def dog_prefSf(modParams, dog_model=2, all_sfs=numpy.logspace(-1, 1, 11)):
   ''' Compute the preferred SF given a set of DoG [or in the case of dog_model==0, not DoG...) parameters
@@ -1789,6 +1820,7 @@ def jl_get_metric_byCon(jointList, metric, conVal, disp, conTol=0.02):
 
 def blankResp(cellStruct, expInd, spikes=None, spksAsRate=False, returnRates=False):
     ''' optionally, pass in array of spikes (by trial) and flag for whether those spikes are rates or counts over the whole trial
+        mu/std_err are ALWAYS rates
     '''
     # works for all experiment variants (checked 08.20.19)
     if 'sfm' in cellStruct:

plot_descr.py
@@ -101,10 +101,16 @@ if rvcAdj == 0:
   rvcFlag = '_f0';
 else:
   rvcFlag = '';
+rvcSuff = hf.rvc_mod_suff(rvcMod);
 rvcBase = '%s%s' % (rvcBase, rvcFlag);
 spikes_rate = hf.get_adjusted_spikerate(trialInf, cellNum, expInd, data_loc, rvcBase, rvcMod=rvcMod, descrFitName_f0 = fLname, baseline_sub=False);
+# let's also get the baseline
+if f1f0rat < 1:
+  baseline_resp = hf.blankResp(trialInf, expInd, spikes=spikes_rate, spksAsRate=True)[0];
+else:
+  baseline_resp = None;
 
-###
+### DEPRECATE???
 # now, we take into account that we fit the responses re-centered such that they are non-negative
 # i.e. if the adjusted responses were negative,
 #   we previuosly added the lowest value (i.e. most negative response) plus an additional scalar (0.1)
@@ -294,7 +300,7 @@ for d in range(nDisps):
       sns.despine(ax=dispAx[d][i], offset=10, trim=False); 
 
       dispAx[d][i].set_ylabel('resp above baseline (sps)');
-      dispAx[d][i].set_title('D%02d - sf tuning');
+      dispAx[d][i].set_title('D%02d - sf tuning' % (d+1));
       dispAx[d][i].legend(); 
 
 saveName = "/allCons_cell_%03d.pdf" % (cellNum)
@@ -391,7 +397,7 @@ for d in range(nDisps):
     fRVC.append(fCurr);
     rvcAx.append(rvcCurr);
     
-    fCurr.suptitle('%s #%d (f1f0 %.2f)' % (cellType, cellNum-1, f1f0rat));
+    fCurr.suptitle('%s #%d (f1f0 %.2f)' % (cellType, cellNum, f1f0rat));
 
     for sf in range(n_v_sfs):
         row_ind = int(sf/n_cols);
@@ -408,7 +414,7 @@ for d in range(nDisps):
 
 	# organize (measured) responses
         resp_curr = np.reshape([respMean[d, sf_ind, v_cons]], (n_cons, ));
-        respPlt = rvcAx[plt_x][plt_y].plot(all_cons[v_cons], np.maximum(resp_curr, 0.1), '-', clip_on=False, label='data');
+        respPlt = rvcAx[plt_x][plt_y].plot(all_cons[v_cons], np.maximum(resp_curr, 0.1), '-', clip_on=False, label='data', color=dataClr);
 
  	# RVC descr model - TODO: Fix this discrepancy between f0 and f1 rvc structure? make both like descrFits?
         if rvcAdj == 1: # i.e. _f1 or non-"_f0" flag on rvcFits
@@ -417,13 +423,17 @@ for d in range(nDisps):
           prms_curr = rvcFits['params'][d][sf_ind]; 
         if rvcMod == 1 or rvcMod == 2: # naka-rushton/peirce
           rvcResps = hf.naka_rushton(cons_plot, prms_curr)
+          c50 = prms_curr[-2]; # second to last entry
         elif rvcMod == 0: # i.e. movshon form
           rvcResps = rvcModel(*prms_curr, cons_plot);
+          c50 = prms_curr[-1]; # last entry is c50
         # TODO: do you want to do the max(x, 0.1)???
         rvcAx[plt_x][plt_y].plot(cons_plot, np.maximum(rvcResps, 0.1), color=modClr, \
           alpha=0.7, clip_on=False, label=modTxt);
-        c50 = prms_curr[-1]; # last entry is c50
         rvcAx[plt_x][plt_y].plot(c50, 0.1, 'v', label='c50', color=modClr);
+        # now, let's also plot the baseline, if complex cell
+        if baseline_resp is not None: # i.e. complex cell
+          rvcAx[plt_x][plt_y].axhline(baseline_resp, color='k', linestyle='dashed');
 
         rvcAx[plt_x][plt_y].set_xscale('log', basex=10); # was previously symlog, linthreshx=0.01
         if col_ind == 0:
@@ -440,7 +450,7 @@ for d in range(nDisps):
         rvcAx[plt_x][plt_y].tick_params(width=2, length=8, which='minor', direction='out'); # minor ticks, too...
 
 saveName = "/cell_%03d.pdf" % (cellNum)
-full_save = os.path.dirname(str(save_loc + 'CRF%s/' % rvcFlag));
+full_save = os.path.dirname(str(save_loc + 'CRF%s%s/' % (rvcSuff, rvcFlag)));
 if not os.path.exists(full_save):
   os.makedirs(full_save);
 pdfSv = pltSave.PdfPages(full_save + saveName);
@@ -478,7 +488,7 @@ for d in range(nDisps):
         plot_resp = respMean[d, sf_ind, v_cons];
 
         line_curr, = crfAx[d][0].plot(all_cons[v_cons][plot_resp>1e-1], plot_resp[plot_resp>1e-1], '-o', color=col, \
-                                      clip_on=False, label = con_str);
+                                      clip_on=False, label=con_str);
         lines_log.append(line_curr);
 
         # now RVC model [1]
@@ -510,7 +520,7 @@ for d in range(nDisps):
       crfAx[d][i].legend();
 
 saveName = "/allSfs_cell_%03d.pdf" % (cellNum)
-full_save = os.path.dirname(str(save_loc + 'CRF%s/' % rvcFlag));
+full_save = os.path.dirname(str(save_loc + 'CRF%s%s/' % (rvcSuff, rvcFlag)));
 if not os.path.exists(full_save):
   os.makedirs(full_save);
 pdfSv = pltSave.PdfPages(full_save + saveName);