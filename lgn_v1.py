354ca89525b1e32b7b04177a411aa83a53d8d8a2
Paul Levy <paul.gerald.levy@gmail.com>
2020-01-31 19:34:21 -0500

    added lgn_v1 func to create LGN lattice, and V1 neuron; next, create fn to sample V1 neuron

--- lgn_v1.py
+++ lgn_v1.py
@@ -22,36 +22,41 @@ plt.style.use('https://raw.githubusercontent.com/paul-levy/SF_diversity/master/p
 
 # Functions:
 ########
-### I. Ringach, 2004 model
+### I. Ringach, 2004 model - in the below section, all default values are from Ringach (2004) unless otherwise noted
+####### loc_x: give X coordinates of RGC location
+####### loc_y: give Y ...
+####### rgc_lattice: create the full RGC lattice 
+####### lgn_lattice: given the RGC locations, resample (as specified) to create LGN lattice
+####### v1_cell: creates an example V1 neuron; will create RGC/LGN lattice if not passed in; otherwise, just samples existing lattice
 ########
 
-def loc_x(sc_factor=1/2.0, lm=1, sig_pos=0.155, is_vector=1):
-  ''' lm (lambda) is 1; sig_pos is 0.155; sc_factor=1 in Ringach, 2004
+def loc_x(sc_factor=1/2.0, lmda=1, sig_pos=0.155, is_vector=1):
+  ''' lmda (lambda) is 1; sig_pos is 0.155; sc_factor=1 in Ringach, 2004
       -- but sc_factor=1/2 gives better approx. of results in that paper
       default is to vectorize the function (for multiple i,j), but can be returned for individual i,j values
    '''
   if is_vector == 1:
-    return lambda i, j: lm*sc_factor*(np.add(i,j)) + np.random.normal(loc=0, scale=lm*sig_pos, size=len(i));
+    return lambda i, j: lmda*sc_factor*(np.add(i,j)) + np.random.normal(loc=0, scale=lmda*sig_pos, size=len(i));
   else:
-    return lambda i, j: lm*sc_factor*(np.add(i,j)) + np.random.normal(loc=0, scale=lm*sig_pos, size=1);
+    return lambda i, j: lmda*sc_factor*(np.add(i,j)) + np.random.normal(loc=0, scale=lmda*sig_pos, size=1);
 
-def loc_y(sc_factor=1/2.0, lm=1, sig_pos=0.155, is_vector=1):
-  ''' lm (lambda) is 1; sig_pos is 0.155; sc_factor=1 in Ringach, 2004
+def loc_y(sc_factor=1/2.0, lmda=1, sig_pos=0.155, is_vector=1):
+  ''' lmda (lambda) is 1; sig_pos is 0.155; sc_factor=1 in Ringach, 2004
       -- but sc_factor=1/2 gives better approx. of results in that paper
       default is to vectorize the function (for multiple i,j), but can be returned for individual i,j values
    '''
   if is_vector == 1:
-    return lambda i, j: lm*sc_factor*np.sqrt(3)*(np.subtract(i,j)) + np.random.normal(loc=0, scale=lm*sig_pos, size=len(i));
+    return lambda i, j: lmda*sc_factor*np.sqrt(3)*(np.subtract(i,j)) + np.random.normal(loc=0, scale=lmda*sig_pos, size=len(i));
   else:
-    return lambda i, j: lm*sc_factor*np.sqrt(3)*(np.subtract(i,j)) + np.random.normal(loc=0, scale=lm*sig_pos, size=1);
+    return lambda i, j: lmda*sc_factor*np.sqrt(3)*(np.subtract(i,j)) + np.random.normal(loc=0, scale=lmda*sig_pos, size=1);
 
-def rgc_lattice(steps, sc_factor=1/2.0, lm=1, sig_pos=0.155, is_vector=1):
+def rgc_lattice(steps, sc_factor=1/2.0, lmda=1, sig_pos=0.155, is_vector=1):
   ''' steps: +/- # integer steps for "i" and "j"
       -- returns the lists for on, off coordinate pairs
   '''
   # how will we compute the x and y coordinates?
-  lx = loc_x(sc_factor, lm, sig_pos, is_vector);
-  ly = loc_y(sc_factor, lm, sig_pos, is_vector);
+  lx = loc_x(sc_factor, lmda, sig_pos, is_vector);
+  ly = loc_y(sc_factor, lmda, sig_pos, is_vector);
 
   # create the sampling grid
   xs = np.arange(-steps, steps+1);
@@ -73,7 +78,7 @@ def lgn_lattice(on_locs, off_locs, resample_factor=0):
   ''' given RGC on/off locations, resample the locations N*"resample_factor" times and add to the existing RGC locs to make the LGN layer
       -- in ringach, 2004, resample_factor is 1.5 (s.t. LGN has 2.5x RGC neurons)
       -- but that is for cat, and in primate, we want 0x!
-      returns: list of on,off coordinate pairs for LGN layer
+      returns: list of (x,y) coordinate pairs for LGN layer, and on/off indices
   '''
 
   all_locs = np.vstack((on_locs, off_locs));
@@ -92,4 +97,74 @@ def lgn_lattice(on_locs, off_locs, resample_factor=0):
   on_inds = np.where(LGN_ids == 1)[0];
   off_inds = np.where(LGN_ids == 0)[0];
 
-  return LGN_locs[on_inds], LGN_locs[off_inds];
+  return LGN_locs, LGN_ids, on_inds, off_inds;
+
+def v1_cell(v1_loc = None, LGN_locs=None, LGN_ids=None, nsteps=12, lmda=1, ctrMult=0.7, pmax=0.85, connMult=0.97, smax=1, synMult=1.1, relToLmda=1, near_thresh=3):
+  ''' The default values here (as above) are from Ringach (2004)
+      - pick a V1 location - then, compute all of the LGN neurons with a RF whose distance is less than "near_thresh" from 
+          that V1 location
+      - from there, we compute if the cell is connected (based on "p" eq. above) and assign a strength (from "s" above)
+      --- note: if LGN_locs/LGN_ids are None, we'll compute a default-parameter LGN lattice
+      --- note: sig_ctr (size of LGN fields), sig_conn (std specyfing fall-off of conxn prob.), sig_syn (as sig_conn, but for syn. strength) 
+                 are given relative to lmda as default (but can be specified otherwise, in which case we pass in the values directly)
+      --- note: near_thresh (within what distance do we consider connections), bndsMult (sample the resultant lattice out to near_thresh*bndsMult units),
+                 and n_samps have been determined by trial-and-error to work well in most use cases
+  '''
+  
+  ### overhead - set up the parameters which determine the 
+  if relToLmda == 1: # default
+    sig_ctr = ctrMult*lmda;
+    sig_conn = connMult*sig_ctr;
+    sig_syn = synMult*sig_ctr;
+  else: # we just pass in the values from there
+    sig_ctr = ctrMult;
+    sig_conn = connMult;
+    sig_syn = synMult;
+
+  # sum(dist, 1) means we can pass in list of X and Y coordinates
+  gauss = lambda x, y, scale, sig: scale*np.exp(-(np.sum(np.square(x-y), 1))/(2*np.square(sig)))
+
+  p = lambda x, y: gauss(x, y, pmax, sig_conn) # probability of connection
+  s = lambda x, y: gauss(x, y, smax, sig_syn) # strength of connection
+  
+  ### create LGN lattice if needed
+  if LGN_locs is None or LGN_ids is None: # then create a default-setting lattice
+    onloc, offloc = rgc_lattice(steps=nsteps, lmda=lmda);
+    LGN_locs, LGN_ids, _, _ = lgn_lattice(onloc, offloc);
+
+  ### set V1 location, if not already specified
+  if v1_loc == None:
+    # -- first, random location
+    rand_dist = 0.4*nsteps*np.random.random(); # 0.4 is just a useful distance (stay away from edges)
+    rand_phi = 2*np.pi*np.random.random()-np.pi; # random angle!
+    # now, put into cartesian coordinates
+    xcr, ycr = rand_dist*np.cos(rand_phi), rand_dist*np.sin(rand_phi)
+    v1_loc = [xcr, ycr];
+
+  ### now, create the neuron!
+  # -- compute the distance and find which LGN neurons are near
+  curr_dist = np.sqrt(np.sum(np.square(v1_loc - LGN_locs), 1));
+  LGN_near = np.where(curr_dist < near_thresh)[0]
+
+  # -- compute the connection probability, see which are connected, and compute sign/weight of those connected ones
+  conxn_prob = p(v1_loc, LGN_locs[LGN_near]);
+
+  which_connected = np.where(np.random.binomial(1, conxn_prob))[0];
+  not_connected = np.setdiff1d(np.arange(len(LGN_near)), which_connected);
+  which_on  = np.where(LGN_ids[LGN_near[which_connected]]==1);
+  which_off = np.where(LGN_ids[LGN_near[which_connected]]==0);
+
+  on_locs = LGN_locs[LGN_near[which_connected[which_on]]];
+  off_locs = LGN_locs[LGN_near[which_connected[which_off]]];
+
+  on_str = s(v1_loc, on_locs);
+  off_str = s(v1_loc, off_locs);
+
+  locations = (on_locs, off_locs);
+  syn_strs = (on_str, off_str);
+
+  return locations, syn_strs;
+
+def v1_sample(locs, syn_strs, bndsMult=70, n_samps=1001):
+  ''' with a V1 neuron created, now sample it to determine inputs/tuning
+  '''
