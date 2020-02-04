5b2c17ca4383e13428a8e52effa334df17854209
Paul Levy <paul.gerald.levy@gmail.com>
2020-02-03 23:42:04 -0500

    finished converting Ringach model into functions, updated .ipynb accordingly

--- lgn_v1.py
+++ lgn_v1.py
@@ -4,6 +4,8 @@ from scipy.ndimage import gaussian_filter as gauss_filt
 import itertools
 import os
 
+import pdb
+
 import warnings
 warnings.filterwarnings('once')
 
@@ -28,7 +30,9 @@ plt.style.use('https://raw.githubusercontent.com/paul-levy/SF_diversity/master/p
 ####### rgc_lattice: create the full RGC lattice 
 ####### lgn_lattice: given the RGC locations, resample (as specified) to create LGN lattice
 ####### v1_cell: creates an example V1 neuron; will create RGC/LGN lattice if not passed in; otherwise, just samples existing lattice
-########
+####### v1_sample: sample the v1_cell to get the evaluated field and inputs
+####### analyze_filt: given a V1 cell, make the fourier transform of the spatial RF; find the peak response; measure ori/sf tuning
+#######
 
 def loc_x(sc_factor=1/2.0, lmda=1, sig_pos=0.155, is_vector=1):
   ''' lmda (lambda) is 1; sig_pos is 0.155; sc_factor=1 in Ringach, 2004
@@ -104,6 +108,8 @@ def v1_cell(v1_loc = None, LGN_locs=None, LGN_ids=None, nsteps=12, lmda=1, ctrMu
       - pick a V1 location - then, compute all of the LGN neurons with a RF whose distance is less than "near_thresh" from 
           that V1 location
       - from there, we compute if the cell is connected (based on "p" eq. above) and assign a strength (from "s" above)
+      - RETURN: the locations and strengths of the connected neurons (evaluating is a separate function, "v1_sample") AND prms
+      -           prms.sig_ctr/prms.sig_conn/prms.sig_syn/prms.lmda/prms.near_thresh
       --- note: if LGN_locs/LGN_ids are None, we'll compute a default-parameter LGN lattice
       --- note: sig_ctr (size of LGN fields), sig_conn (std specyfing fall-off of conxn prob.), sig_syn (as sig_conn, but for syn. strength) 
                  are given relative to lmda as default (but can be specified otherwise, in which case we pass in the values directly)
@@ -163,8 +169,140 @@ def v1_cell(v1_loc = None, LGN_locs=None, LGN_ids=None, nsteps=12, lmda=1, ctrMu
   locations = (on_locs, off_locs);
   syn_strs = (on_str, off_str);
 
-  return locations, syn_strs;
+  prms = dict();
+  prms['lmda'] = lmda;
+  prms['sig_ctr'] = sig_ctr;
+  prms['sig_conn'] = sig_conn;
+  prms['sig_syn'] = sig_syn;
+  prms['near_thresh'] = near_thresh;
+  prms['v1_loc'] = v1_loc;
+  prms['gauss'] = gauss;
 
-def v1_sample(locs, syn_strs, bndsMult=70, n_samps=1001):
+  return locations, syn_strs, prms;
+
+def v1_sample(locs, syn_strs, prms, bndsMult=70, n_samps=1001):
   ''' with a V1 neuron created, now sample it to determine inputs/tuning
   '''
+  # unpack the needed parameters from prms
+  sig_ctr = prms['sig_ctr'];
+  near_thresh = prms['near_thresh'];
+  v1_loc = prms['v1_loc'];
+  gauss = prms['gauss'];
+
+  # set up the coordinates
+  bnds = bndsMult*near_thresh;
+  xs = np.linspace(-bnds, bnds, n_samps) + v1_loc[0];
+  ys = np.linspace(-bnds, bnds, n_samps) + v1_loc[1];
+  xcoor, ycoor = np.meshgrid(xs, ys);
+  all_coords = np.transpose(np.vstack((xcoor.ravel(), ycoor.ravel())));
+
+  lgn_field = np.zeros(len(all_coords), )
+  on_sum = np.zeros_like(lgn_field);
+  off_sum = np.zeros_like(lgn_field)
+  on_fields = [];
+  off_fields = [];
+  all_fields = (on_fields, off_fields);
+  ids = (1, 0); # on--off
+
+  for all_field, lcs, syn_str, id_curr in zip(all_fields, locs, syn_strs, ids):
+    for i in range(len(lcs)):
+      curr_loc = lcs[i];
+      curr_field = gauss(curr_loc, all_coords, syn_str[i], sig_ctr)
+
+      all_field.append(curr_field);
+
+      if id_curr == 1:
+        on_sum = on_sum + curr_field;
+      elif id_curr == 0:
+        off_sum = off_sum + curr_field;
+
+  sums = (on_sum, off_sum);
+  tot_field = sums[0] - sums[1]; # i.e. subtract the "off" from the "on"
+
+  return tot_field, sums, all_fields;
+
+################
+
+def analyze_filt(v1_field, lgn_field, n_samps, bounds):
+  ''' given the (1D) V1 field (and n_samps/bounds), compute the 2D Fourier transform
+      - from this, we can compute:
+      -- pref. ori, ori. tuning
+      -- pref. sf, sf. tuning
+      -- LGN tuning evaluated at the same SF/ORI values
+      -- reconstruct the (spatial) V1 field as a check
+  '''
+
+  v1_plot = np.reshape(v1_field, (n_samps, n_samps)); # rearrange the field in a 2D plot
+  ft = np.fft.fft2(v1_plot)
+  # - and what frequencies?? well, [-n_samps/2, n_samps/2]*freq_scale
+  samp_step = np.round(2*bounds/n_samps, 3);
+  freq_scale = samp_step;
+  freqs = freq_scale*np.linspace(-(n_samps/2), n_samps/2, n_samps)
+  xfreq, yfreq = np.meshgrid(freqs, freqs);
+  v1_ft = np.abs(np.fft.fftshift(ft));
+  filt_peak = np.argmax(v1_ft);
+  xmax, ymax = np.unravel_index(filt_peak, v1_ft.shape, order='F')
+  xfr_peak, yfr_peak = freqs[xmax], freqs[ymax];
+  peak_sf = np.sqrt(np.square(xfr_peak) + np.square(yfr_peak));
+  peak_ori = np.mod(np.round(hf.angle_xy([xfr_peak], [yfr_peak])[0], 3), 180);
+
+  #####
+  # What is the tuning? i.e. SF and ORI?
+  #####
+  all_freqs = np.transpose(np.vstack((xfreq.ravel(), yfreq.ravel())));
+  # -- first, get the valid ori
+  oris = np.mod(hf.angle_xy(all_freqs[:, 0], all_freqs[:,1]), 180); # with no D.S., mod by 180
+  val_oris = np.where(np.mod(np.abs(oris-peak_ori), 180)<1)[0]; # np.deg2rad(5e-2) if radians
+  # -- then, the valid sfs
+  sfs = np.sqrt(np.square(all_freqs[:, 0])+np.square(all_freqs[:, 1]))
+  val_sfs = np.where(np.abs(sfs-peak_sf)<0.5*samp_step)[0];
+
+  #####
+  ## NOW -- plot sf tuning measurements (fixed ori)
+  #####
+  sf_bound = freq_scale*np.maximum(15*peak_sf, .55*freqs[-1]); # heuristic...should be updated
+  plt_oris = val_oris[np.where(sfs[val_oris]<1.25*sf_bound)]
+  sf_vals = sfs[plt_oris]
+  sf_curve = v1_ft[[x for x in np.unravel_index(plt_oris, v1_ft.shape, order='C')]]
+  sf_order = np.argsort(sf_vals);
+  sf_resp_norm = np.divide(sf_curve[sf_order], np.max(sf_curve));
+  ## -- and get the LGN filter tuning, too
+  indiv_shp = np.reshape(lgn_field, v1_plot.shape) # lgn_field is just any of the LGN fields - they're identical!
+  indiv_ft = np.fft.fft2(indiv_shp)
+  indiv_plot = np.abs(np.fft.fftshift(indiv_ft));
+  # ---- and evaluate at the same locations
+  lgn_curve = indiv_plot[[x for x in np.unravel_index(plt_oris, v1_ft.shape, order='F')]]
+  lgn_resp_norm = np.divide(lgn_curve[sf_order], np.max(lgn_curve));
+  #####
+  ## THEN -- plot ori tuning measurements (fixed SF)
+  #####
+  core_oris = np.where(np.array([np.round(freqs[y], 3) for x,y in [np.unravel_index(d, v1_ft.shape, order='F') for d in val_sfs]])>=0);
+  plt_sfs = val_sfs[core_oris]
+  ori_vals = oris[plt_sfs]
+  ori_curve = v1_ft[[x for x in np.unravel_index(plt_sfs, v1_ft.shape, order='C')]]
+  ori_order = np.argsort(ori_vals);
+  ori_resp_norm = np.divide(ori_curve[ori_order], np.max(ori_curve));
+
+  #####
+  # organize the tuning
+  #####
+  sf_tune = dict();
+  sf_tune['peak'] = peak_sf;
+  sf_tune['curve'] = sf_resp_norm;
+  sf_tune['sf_vals'] = sf_vals[sf_order]
+  sf_tune['ori_vals'] = plt_oris; # why? well, the SF tuning is evaluated at a particular orientation - which values?
+  ori_tune = dict();
+  ori_tune['peak'] = peak_ori;
+  ori_tune['curve'] = ori_resp_norm;
+  ori_tune['ori_vals'] = ori_vals[ori_order]
+  ori_tune['sf_vals'] = plt_sfs; # why? well, the ORI tuning is evaluated at a particular SF - which values?
+  # -- and, don't forget, the LGN tuning!
+  lgn_tune = dict();
+  lgn_tune['sf_vals'] = sf_vals[sf_order];
+  lgn_tune['curve'] = lgn_resp_norm;
+
+  # --- show that we can reconstruct it back into the spatial domain
+  ift = (np.fft.ifft2(ft))
+  v1_reconstruct = np.real(np.sign(ift))*np.abs(ift);
+
+  return v1_ft, sf_tune, ori_tune, lgn_tune, v1_reconstruct;
