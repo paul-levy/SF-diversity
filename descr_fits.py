fixed bad error assuming fixed stimulus duration during f1f0 calculation (mattered for older non-half int experiments)

descr_fits.py
@@ -337,7 +337,7 @@ def rvc_adjusted_fit(cell_num, expInd, data_loc, descrFitName_f0=None, rvcName=r
 
   if to_save:
     np.save(data_loc + rvcNameFinal, rvcFits);
-    print('saving rvc fit for cell %d, disp %d' % (cell_num, disp));
+    print('saving rvc fit [%s] for cell %d, disp %d' % (rvcNameFinal, cell_num, disp));
 
   if returnMod:
     return rvc_model, all_opts, all_conGains, adjMeans;

helper_fcns.py
@@ -855,6 +855,8 @@ def compute_f1_byTrial(cellStruct, expInd, whichSpikes=1, binWidth=1e-3):
       Use: Will be used in conjunction with the adjust_f1_byTrial
   '''
   np = numpy;
+
+  stimDur = get_exp_params(expInd).stimDur;
  
   if 'sfm' in cellStruct: 
     data = cellStruct['sfm']['exp']['trial'];
@@ -864,11 +866,11 @@ def compute_f1_byTrial(cellStruct, expInd, whichSpikes=1, binWidth=1e-3):
   # first, make the PSTH for each trial (only extract output [0], which is the psth)
   if whichSpikes == 1:
     try:
-      psth = make_psth(data['spikeTimesGLX']['spikeTimes'], 1e-3, 2)[0];
+      psth = make_psth(data['spikeTimesGLX']['spikeTimes'], binWidth, stimDur)[0]; # CORRECTED ON 21.08.26
     except:
       whichSpikes = 0;
   if whichSpikes == 0:
-    psth = make_psth(data['spikeTimes'], 1e-3, 2)[0];
+    psth = make_psth(data['spikeTimes'], binWidth, stimDur)[0]; # CORRECTED ON 21.08.26
   # then, get the stimulus TF values
   all_tf = np.vstack(data['tf']);
   # with this, we can extract the F1 rates at those TF values
@@ -938,6 +940,7 @@ def adjust_f1_byTrial(cellStruct, expInd, dir=-1, whichSpikes=1, binWidth=1e-3, 
           resp_proj = np.multiply(r_byTrial[val_trials, :], np.cos(np.deg2rad(phi_mean) - np.deg2rad(phase_rel_stim)));
 
         adjusted_f1_rate[val_trials, :] = resp_proj;
+          
 
   if toSum:
     # then, sum up the valid components per stimulus component

jl_create.py
@@ -99,7 +99,7 @@ np.save(base_dir + 'jointList_V1_wMods_%s_vT%d_dvT%d' % (suffix, varExplThresh, 
 #expDirs = ['LGN/']
 #expNames = ['dataList.npy']
 expDirs = ['LGN/', 'LGN/sach/'];
-expNames = ['dataList.npy', 'sachData.npy']
+expNames = ['dataList_210721.npy', 'sachData.npy']
 #expDirs = ['LGN/sach/'];
 #expNames = ['sachData.npy']
 
@@ -117,16 +117,16 @@ fitNamesFlat = ['%s_flat_chiSq.npy' % fitBase];
 #dogNames = ['descrFits_s210304_sach_sach.npy'];
 #descrNames = ['descrFits_s210304_sqrt_flex.npy'];
 dogMod = 2; # 1 (sach) or 2 (Tony)
-dogNames = ['descrFits_210520_sqrt_tony.npy', 'descrFits_s210520_sqrt_tony.npy'];
+dogNames = ['descrFits_210721_sqrt_tony.npy', 'descrFits_s210721_sqrt_tony.npy'];
 #dogNames = ['descrFits_210304_sach_sach.npy', 'descrFits_s210304_sach_sach.npy'];
 descrMod = 0; # which model for the diff. of gauss fits (0/1/2: flex/sach/tony)
-descrNames = ['descrFits_210520_sqrt_flex.npy', 'descrFits_s210520_sqrt_flex.npy'];
+descrNames = ['descrFits_210721_sqrt_flex.npy', 'descrFits_s210721_sqrt_flex.npy'];
 #dogNames = ['descrFits_191023_sach_sach.npy'];
 #descrNames = ['descrFits_200507_sqrt_flex.npy'];
 
 #rvcNames = ['rvcFits_191023_pos.npy']; 
 #rvcMods = [0]; # 0-mov (blank); 1-Nakarushton (NR); 2-Peirce (peirce)
-rvcNames = ['rvcFits_210520_pos.npy', 'rvcFits_210520.npy']; 
+rvcNames = ['rvcFits_210721_pos.npy', 'rvcFits_210721.npy']; 
 rvcMods = [0,0]; # 0-mov (blank); 1-Nakarushton (NR); 2-Peirce (peirce)
 #rvcNames = ['rvcFits_210304.npy']; 
 #rvcMods = [0]; # 0-mov (blank); 1-Nakarushton (NR); 2-Peirce (peirce)
@@ -158,8 +158,8 @@ sf_range = [0.01, 10]; # allowed values of 'mu' for fits - see descr_fit.py for 
 jointList = hf.jl_create(base_dir, expDirs, expNames, fitNamesWght, fitNamesFlat, descrNames, dogNames, rvcNames, rvcMods, dog_varExplThresh=dog_varExplThresh, descrMod=descrMod, dogMod=dogMod)
 
 from datetime import datetime
-suffix = '210518'
-#suffix = datetime.today().strftime('%y%m%d')
+#suffix = '210518'
+suffix = datetime.today().strftime('%y%m%d')
 
 np.save(base_dir + 'jointList_LGN_%s_vT%d_dvT%d' % (suffix, varExplThresh, dog_varExplThresh), jointList)
 